9db514a84ce8f3a69397e3242e2d420031be6163
Rachael Thompson <rachael.elizabeth.thompson@gmail.com>
2019-01-31 07:47:13 -0600

    updated models still need to add 3 more- comments, map, and calendar

--- _FinalProject/_FinalProject/Models/User.cs
+++ _FinalProject/_FinalProject/Models/User.cs
@@ -4,7 +4,7 @@ namespace _FinalProject.Models
     public class User
     {
         
-        public string UserId { get; set; }
+        public string Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string ImageURL { get; set; }
--- _FinalProject/_FinalProject/Models/UsersByRobin.cs
+++ _FinalProject/_FinalProject/Models/UsersByRobin.cs
@@ -3,12 +3,12 @@ namespace _FinalProject.Models
 {
     public class UsersByRobin
     {
+        public int Id { get; set; }
+        //unique attributes
+        public bool Admin { get; set; }
         //Relationships
         public string UserId { get; set; }
         public int RobinID { get; set; }
-
-        //unique attributes
-        public bool Admin { get; set; }
 
     }
 }
