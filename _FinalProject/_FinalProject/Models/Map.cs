3b935c4cc6af55cef34a9f0f8e967bc8c0e8c218
Rachael <rachael.elizabeth.thompson@speakwrite.com>
2019-02-02 15:29:21 -0600

    added the rest of the models

---
+++ _FinalProject/_FinalProject/Models/Map.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _FinalProject.Models
+{
+    public class Map
+    {
+        public int Id { get; set; }
+       public double Latutude { get; set; }
+        public double  Longitute { get; set; }
+
+        //relationships
+        public string UserId { get; set; }
+        public int RobinId { get; set; }
+
+    }
+}
---
+++ _FinalProject/_FinalProject/Models/Post.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _FinalProject.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime DeletedDate { get; set; }
+
+        //relationships
+        public string UserId { get; set; }
+        public int RobinId { get; set; }
+    }
+}
