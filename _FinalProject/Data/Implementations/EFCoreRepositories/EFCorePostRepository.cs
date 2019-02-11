99b5949151aa70d2568e711e61a60c9f465cb9d9
Rachael <rachael.elizabeth.thompson@speakwrite.com>
2019-02-10 19:35:54 -0600

    created the classes for these repos

---
+++ _FinalProject/Data/Implementations/EFCoreRepositories/EFCorePostRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.Implementations.EFCoreRepositories
+{
+    class EFCorePostRepository
+    {
+    }
+}
---
+++ _FinalProject/Data/Implementations/MockRepositories/MockRobinRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.Implementations.MockRepositories
+{
+    class MockRobinRepository
+    {
+    }
+}
