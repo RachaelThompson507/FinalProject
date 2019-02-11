d736bed790cff9f77dfb04c5073d84e1a67987ea
Rachael <rachael.elizabeth.thompson@speakwrite.com>
2019-02-10 19:31:30 -0600

    completed

--- _FinalProject/Data/Implementations/EFCoreRepositories/EFCoreLetterRepository.cs
+++ _FinalProject/Data/Implementations/EFCoreRepositories/EFCoreLetterRepository.cs
@@ -1,7 +1,9 @@
-﻿using _FinalProject.Model.Models;
+﻿using _FinalProject.Data.Context;
+using _FinalProject.Model.Models;
 using Data.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Data.Implementations.EFCoreRepositories
@@ -10,32 +12,60 @@ namespace Data.Implementations.EFCoreRepositories
     {
         public Letter Create(Letter newLetter)
         {
-            throw new NotImplementedException();
+            using (var db = new FinalProjectDBContext())
+            {
+                db.Letters.Add(newLetter);
+                db.SaveChanges();
+                return newLetter;
+            }
         }
 
         public bool DeleteById(int letterId)
         {
-            throw new NotImplementedException();
+            using (var db = new FinalProjectDBContext())
+            {
+                var letter = GetById(letterId);
+                db.Letters.Remove(letter);
+                db.SaveChanges();
+                return true;
+            }
         }
 
         public Letter GetById(int letterId)
         {
-            throw new NotImplementedException();
+            using (var db = new FinalProjectDBContext())
+            {
+                return db.Letters.Single(l => l.Id == letterId);
+            }
         }
 
         public ICollection<Letter> GetRobinById(int robinId)
         {
-            throw new NotImplementedException();
+            using (var db = new FinalProjectDBContext())
+            {
+                var robinLetter = db.Letters.Where(c => c.RobinId == robinId).ToList() as ICollection<Letter>;
+                return robinLetter;
+            }
         }
 
         public ICollection<Letter> GetUserById(string userId)
         {
-            throw new NotImplementedException();
+            using (var db = new FinalProjectDBContext())
+            {
+                var userLetter = db.Letters.Where(c => c.UserId == userId).ToList() as ICollection<Letter>;
+                return userLetter;
+            }
         }
 
         public Letter Update(Letter updatedLetter)
         {
-            throw new NotImplementedException();
+            using (var db = new FinalProjectDBContext())
+            {
+                var update = GetById(updatedLetter.Id);
+                db.Entry(update).CurrentValues.SetValues(updatedLetter);
+                db.SaveChanges();
+                return update;
+            }
         }
     }
 }
