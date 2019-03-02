4e0dbb5d5ec8cdaca71e8450137fe473ed20fc95
Rachael <rachael.elizabeth.thompson@speakwrite.com>
2019-03-01 21:35:53 -0600

    changesview

---
+++ _FinalProject/Service/Services/EventService.cs
@@ -0,0 +1,57 @@
+﻿using Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Service.Services
+{
+    public interface IEventService
+    {
+        //Create
+        Event Create(Event newEvent);
+
+        //Read
+        Event GetById(int eventId);
+        ICollection<Event> GetUserById(string userId);
+        ICollection<Event> GetRobinById(int robinId);
+        ICollection<Event> GetCalendarById(int calendarId);
+
+        //Update
+        Event Update(Event updatedEvent);
+
+        //Delete
+        bool DeleteById(int eventId);
+    }
+
+    public class EventService : IEventService
+    {
+        private readonly IEventService _eventService;
+
+        public EventService(IEventService eventService) =>
+            _eventService = eventService;
+
+        public Event Create(Event newEvent) =>
+            _eventService.Create(newEvent);
+
+
+        public bool DeleteById(int eventId) =>
+            _eventService.DeleteById(eventId);
+
+        public Event GetById(int eventId) =>
+            _eventService.GetById(eventId);
+
+        public ICollection<Event> GetCalendarById(int calendarId) =>
+            _eventService.GetCalendarById(calendarId);
+
+        public ICollection<Event> GetRobinById(int robinId) =>
+            _eventService.GetRobinById(robinId);
+
+
+        public ICollection<Event> GetUserById(string userId) =>
+            _eventService.GetUserById(userId);
+
+        public Event Update(Event updatedEvent) =>
+            _eventService.Update(updatedEvent);
+    }
+
+}
