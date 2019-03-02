4dca944ec64dfc38fffa79f81e57e5abbd95865f
Rachael <rachael.elizabeth.thompson@speakwrite.com>
2019-03-02 11:27:59 -0600

    working on figuring out login for users and how I want them to login

---
+++ _FinalProject/_FinalProject/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using _FinalProject.Model.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly SignInManager<User> _signInManager;
+        private readonly List<IdentityRole> _roles;
+
+        public UserController(UserManager<User> userManager, RoleManager<IdentityRole> roleManager,
+        SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _signInManager = signInManager;
+            //call Role Table one time
+            _roles = _roleManager.Roles.ToList();
+        }
+        
+        [HttpGet]
+        public IActionResult CreateUser()
+        {
+            //only allowing one role to be created this way
+            var userVM = new ViewModels.CreateUserViewModel
+            {
+                Roles = new Select(_roles)
+            };
+            return View(userVM);
+        }
+    }
+}
---
+++ _FinalProject/_FinalProject/ViewModels/CreateUserViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.ViewModels
+{
+    public class CreateUserViewModel
+    {
+        [EmailAddress, Required]
+        public string Email { get; set; }
+
+        [DataType(DataType.Password), Required]
+        public string Password { get; set; }
+        
+        [DataType(DataType.Password), Compare("Password", ErrorMessage = "Password Fields Do Not Match"), Required]
+        public string ConfirmPassword { get; set; }
+
+        [Required, Display(Description =("First Name"))]
+        public string FirstName { get; set; }
+
+        [Required, Display(Description = ("Last Name"))]
+        public string LastName { get; set; }
+    }
+}
