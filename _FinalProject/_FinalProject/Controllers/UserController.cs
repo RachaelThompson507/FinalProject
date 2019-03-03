6bac8db32ed4d39a96bbe17d87cfc903d345eb35
Rachael <rachael.elizabeth.thompson@speakwrite.com>
2019-03-03 11:56:56 -0600

    Authorize user complete

--- _FinalProject/_FinalProject/Controllers/UserController.cs
+++ _FinalProject/_FinalProject/Controllers/UserController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using _FinalProject.Model.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Web.ViewModels;
@@ -72,6 +73,7 @@ namespace Web.Controllers
             return RedirectToAction("Index", "Home");
         }
 
+        [Authorize(Roles ="GeneralUser")]
         public IActionResult Index()
         {
             return View();
